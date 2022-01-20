e11a2e85aa7c26d49d446f0433930d95312d2537
rocha19 <rochaarqueologia@outlook.com>
2022-01-20 12:59:43 -0300

    apredendo router do vue.js

---
+++ vuecomrouter/src/views/Teste.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input type="text" name="email" placeholder="nome" id="nome">
+        <input type="text" name="email" id="email" placeholder="email">
+        <button>TESTE</button>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    created(){
+        // this.parametroDaRota = this.$route.params.dado;
+    },
+    dado(){
+        return {
+            paramatroDaRota: "nada"
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+#email{
+    margin-top: 1%;
+    margin-bottom: 1%;
+}
+
+</style>
